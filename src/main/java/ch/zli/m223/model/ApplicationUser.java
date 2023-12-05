0eb8b3297a2f9a0b5f04e890d3e7561dd2c00854
cedi1136 <cedric-wagner@gmx.ch>
2023-12-05 15:28:10 +0000

    ApplicationUser change

--- src/main/java/ch/zli/m223/model/ApplicationUser.java
+++ src/main/java/ch/zli/m223/model/ApplicationUser.java
@@ -1,5 +1,8 @@
 package ch.zli.m223.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,20 +10,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import javax.persistence.OneToMany;
 
 @Entity
 @NamedQueries({
-  @NamedQuery(name = "ApplicationUser.findByEmail", query = "SELECT u FROM ApplicationUser u WHERE u.email = :email")
+    @NamedQuery(name = "ApplicationUser.findByEmail", query = "SELECT u FROM ApplicationUser u WHERE u.email = :email")
 })
 public class ApplicationUser {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Schema(readOnly = true)
   private Long id;
-  
+
   @Column(nullable = false, unique = true)
   private String email;
 
@@ -29,6 +30,22 @@ public class ApplicationUser {
 
   @Column
   private String nickname;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @OneToMany(mappedBy = "applicationUser", cascade = CascadeType.ALL)
+  private Set<Booking> bookings;
+
+  public enum UserRole {
+    ADMIN, USER, VISITOR
+  }
+
+  @Column(nullable = false)
+  private UserRole userrole;
 
   public Long getId() {
     return id;
@@ -60,5 +77,37 @@ public class ApplicationUser {
 
   public void setNickname(String nickname) {
     this.nickname = nickname;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public UserRole getUserRole() {
+    return userrole;
+  }
+
+  public void setUserRole(UserRole userrole) {
+    this.userrole = userrole;
+  }
+
+  public Set<Booking> getBookings() {
+    return bookings;
+  }
+
+  public void setBookings(Set<Booking> bookings) {
+    this.bookings = bookings;
   }
 }
